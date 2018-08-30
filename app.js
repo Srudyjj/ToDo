1d4eccd2c1a1240c5bb6cc9c872e7424a76a92a7
Turbinist <srudyjju@gmail.com>
2018-08-30 12:21:44 +0300

    Add storage

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// window.addEventListener("load", function(event) {
-//     console.log("All resources finished loading!");
-// });
-
 const toDo = (function() {
 
   const UIElements = {
@@ -9,18 +5,38 @@ const toDo = (function() {
     taskText: document.getElementById('taskText'),
     addBtn: document.getElementById('addBtn'),
     taskList: document.getElementById('taskList')
-  }
+  };
 
   const clearFields = function() {
     UIElements.taskName.value = '';
     UIElements.taskText.value = '';
-  }
+  };
 
   const warning = function(elem) {
     elem.classList.add("warning");
     setTimeout(() => {
       elem.classList.remove("warning")
     }, 2000);
+  };
+
+  const storage = {
+
+    getItems: function() {
+      let items = {};
+      for (let i = 0; i < localStorage.length; i++) {
+        const name = localStorage.key(i);
+        items[name] = localStorage.getItem(name);
+      }
+      return items;
+    },
+
+    setItem: function(name, description) {
+      localStorage.setItem(String(name), String(description));
+    },
+
+    deleteItem: function(name) {
+      localStorage.removeItem(String(name));
+    }
   }
 
   return {
@@ -28,28 +44,17 @@ const toDo = (function() {
       if (UIElements.taskName.value !== "") {
         const task = `
           <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 pt-2 pb-4">
-            <div class="card">
+            <div status="" class="card">
               <div class="card-body">
                 <h5 class="card-title">${UIElements.taskName.value}</h5>
                 <p class="card-text">${UIElements.taskText.value}</p>
-                <div class="btn-group" role="group" aria-label="Button group">
-                  <div class="btn-group" role="group">
-                    <button id="taskStatus" type="button" class="btn btn-outline-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      Status
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="taskStatus">
-                      <a class="dropdown-item" status="done" href="#">Done</a>
-                      <a class="dropdown-item" status="onProcess" href="#">On Process</a>
-                    </div>
-                  </div>
-                  <button type="button" class="edit btn btn btn-outline-warning">Edit</button>
-                  <button type="button" class="delete btn btn btn-outline-danger">Delete</button>
-                </div>
-              </div>
+                <button type="button" class="delete btn btn btn-outline-danger">Delete</button>
+              </div>  
             </div>
           </div>
         `;
       UIElements.taskList.innerHTML += task;
+      storage.setItem(UIElements.taskName.value, UIElements.taskText.value)
       clearFields();
       } else {
         warning(UIElements.taskName);
@@ -58,34 +63,50 @@ const toDo = (function() {
 
     delete: function(e) {
       if (e.target.classList.contains("delete")) {
-        parent(e.target);
+        removeCard(e.target);
 
-        function parent(elem) {        
+        function removeCard(elem) {        
           if(elem.classList.contains("card")) {
+            const name = elem.querySelector(".card-title").textContent;
+            storage.deleteItem(name);
             return elem.parentElement.remove();
           }
-          parent(elem.parentElement); 
+          removeCard(elem.parentElement); 
         }
       }
     },
 
-    edit: function() {
-      console.log("edit");
-      
-    },
-
-    status: function() {
-      console.log("status");
-    },
-
-    getUI : function() {
+    getUI: function() {
       return UIElements
+    },
+
+    renderElements: function() {
+      const items = storage.getItems();
+
+      for(name in items) {
+        const task = `
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 pt-2 pb-4">
+            <div status="" class="card">
+              <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">${items[name]}</p>
+                <button type="button" class="delete btn btn btn-outline-danger">Delete</button>
+              </div>  
+            </div>
+          </div>
+        `;
+        UIElements.taskList.innerHTML += task;
+      }
     }
   }
 })()
 
 toDo.getUI().addBtn.addEventListener('click', toDo.add);
 toDo.getUI().taskList.addEventListener('click', toDo.delete);
+
+window.addEventListener("load", function() {
+  toDo.renderElements();
+});
 
 
 
