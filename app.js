4c6a18d26030166d5ea3093dd747419bd8b03b25
Turbinist <srudyjju@gmail.com>
2018-09-05 15:24:06 +0300

    Add remover from dom

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ class Form {
     // this.taskStatus
     this.addBtn = document.getElementById("addBtn");
     this.add = this.add.bind(this);
+    this.addListeners = this.addListeners.bind(this);
+    return (this.addListeners())
   }
 
   addListeners() {
     this.addBtn.addEventListener("click", (e) => {
       if (this.taskName.value !== "") {
         this.add();
+        this.taskName.value = "";
+        this.taskDesc.value = "";
       }
       else {
         alert("Fill the Task Name field")
@@ -31,40 +35,56 @@ class Task {
     this.desc = sender.taskDesc.value;
     // this.status = status;
     this.createElement = this.createElement.bind(this);
+    this.removeElement = this.removeElement.bind(this);
+    this.taskElement;
   }
 
   createElement() {
-    return (`
-    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 pt-2 pb-4">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">${this.name}</h5>
-          <p class="card-text">${this.desc}</p>
-          <button type="button" class="delete btn btn btn-outline-danger">Delete</button>
-        </div>  
-      </div>
-    </div>`)
+    this.taskElement = document.createElement("div");
+    this.taskElement.classList.add("col-xs-12", "col-sm-6", "col-md-4", "col-lg-3", "pt-2", "pb-4");
+    const card = document.createElement("div");
+    card.classList.add("card");
+    this.taskElement.appendChild(card);
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    card.appendChild(cardBody);
+    cardBody.innerHTML = `<h5 class="card-title">${this.name}</h5>
+    <p class="card-text">${this.desc}</p>`;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("type", "button");
+    deleteBtn.classList.add("delete", "btn", "btn-outline-danger");
+    deleteBtn.innerText = "Delete";
+    cardBody.appendChild(deleteBtn);
+    deleteBtn.addEventListener("click", (e) => this.removeElement());
+    return (this.taskElement);
   }
 
-  addToDOM(task_list) {
-    task_list.innerHTML += this.createElement();
+  removeElement() {
+    this.mediator.notify(this, "remove");
   }
 }
 
 class Mediator {
   constructor() {
-    this.task_list = document.getElementById("taskList");   
+    this.task_list = document.getElementById("taskList");
+    this.addToDom = this.addToDom.bind(this);   
+  }
+
+  addToDom(HTMLelement) {
+    this.task_list.appendChild(HTMLelement);
   }
 
   notify(sender, message) {
     if (sender instanceof Form && message === "add" ) {  
       const task = new Task(this, sender);
-      task.createElement();    
-      task.addToDOM(this.task_list);
+      const HtmlElement = task.createElement();
+      this.addToDom(HtmlElement);
+    } else if (sender instanceof Task && message === "remove" ) {  
+      sender.taskElement.remove();
     }
   }
 }
 
 const mediator = new Mediator();
 const form = new Form(mediator);
-form.addListeners();
+
