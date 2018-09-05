1b65f4f6bc115467d38486f4a3fb63f173e82ce6
Turbinist <srudyjju@gmail.com>
2018-09-05 14:11:16 +0300

    Add added to DOM

--- app.js
+++ app.js
@@ -1,115 +1,70 @@
-const toDo = (function() {
-
-  const UIElements = {
-    taskName: document.getElementById('taskTitle'),
-    taskText: document.getElementById('taskText'),
-    addBtn: document.getElementById('addBtn'),
-    taskList: document.getElementById('taskList')
-  };
-
-  const clearFields = function() {
-    UIElements.taskName.value = '';
-    UIElements.taskText.value = '';
-  };
-
-  const warning = function(elem) {
-    elem.classList.add("warning");
-    setTimeout(() => {
-      elem.classList.remove("warning")
-    }, 2000);
-  };
-
-  const storage = {
-
-    getItems: function() {
-      let items = {};
-      for (let i = 0; i < localStorage.length; i++) {
-        const name = localStorage.key(i);
-        items[name] = localStorage.getItem(name);
-      }
-      return items;
-    },
-
-    setItem: function(name, description) {
-      localStorage.setItem(String(name), String(description));
-    },
-
-    deleteItem: function(name) {
-      localStorage.removeItem(String(name));
-    }
+class Form {
+  constructor(mediator) {
+    this.mediator = mediator;
+    this.taskName = document.getElementById("taskTitle");
+    this.taskDesc = document.getElementById("taskText");
+    // this.taskStatus
+    this.addBtn = document.getElementById("addBtn");
+    this.add = this.add.bind(this);
   }
 
-  return {
-    add: function() {
-      if (UIElements.taskName.value !== "") {
-        const task = `
-          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 pt-2 pb-4">
-            <div status="" class="card">
-              <div class="card-body">
-                <h5 class="card-title">${UIElements.taskName.value}</h5>
-                <p class="card-text">${UIElements.taskText.value}</p>
-                <button type="button" class="delete btn btn btn-outline-danger">Delete</button>
-              </div>  
-            </div>
-          </div>
-        `;
-      UIElements.taskList.innerHTML += task;
-      storage.setItem(UIElements.taskName.value, UIElements.taskText.value)
-      clearFields();
-      } else {
-        warning(UIElements.taskName);
+  addListeners() {
+    this.addBtn.addEventListener("click", (e) => {
+      if (this.taskName.value !== "") {
+        this.add();
       }
-    },
-
-    delete: function(e) {
-      if (e.target.classList.contains("delete")) {
-        removeCard(e.target);
-
-        function removeCard(elem) {        
-          if(elem.classList.contains("card")) {
-            const name = elem.querySelector(".card-title").textContent;
-            storage.deleteItem(name);
-            return elem.parentElement.remove();
-          }
-          removeCard(elem.parentElement); 
-        }
+      else {
+        alert("Fill the Task Name field")
       }
-    },
+    })
+  }
 
-    getUI: function() {
-      return UIElements
-    },
+  add() {
+    this.mediator.notify(this, "add");
+  }
+}
 
-    renderElements: function() {
-      const items = storage.getItems();
+class Task {
+  constructor(mediator, sender){
+    this.mediator = mediator;
+    this.name = sender.taskName.value;
+    this.desc = sender.taskDesc.value;
+    // this.status = status;
+    this.createElement = this.createElement.bind(this);
+  }
 
-      for(name in items) {
-        const task = `
-          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 pt-2 pb-4">
-            <div status="" class="card">
-              <div class="card-body">
-                <h5 class="card-title">${name}</h5>
-                <p class="card-text">${items[name]}</p>
-                <button type="button" class="delete btn btn btn-outline-danger">Delete</button>
-              </div>  
-            </div>
-          </div>
-        `;
-        UIElements.taskList.innerHTML += task;
-      }
+  createElement() {
+    return (`
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 pt-2 pb-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${this.name}</h5>
+          <p class="card-text">${this.desc}</p>
+          <button type="button" class="delete btn btn btn-outline-danger">Delete</button>
+        </div>  
+      </div>
+    </div>`)
+  }
+
+  addToDOM(task_list) {
+    task_list.innerHTML += this.createElement();
+  }
+}
+
+class Mediator {
+  constructor() {
+    this.task_list = document.getElementById("taskList");   
+  }
+
+  notify(sender, message) {
+    if (sender instanceof Form && message === "add" ) {  
+      const task = new Task(this, sender);
+      task.createElement();    
+      task.addToDOM(this.task_list);
     }
   }
-})()
+}
 
-toDo.getUI().addBtn.addEventListener('click', toDo.add);
-toDo.getUI().taskList.addEventListener('click', toDo.delete);
-
-window.addEventListener("load", function() {
-  toDo.renderElements();
-});
-
-
-
-
-
-
+const mediator = new Mediator();
+const form = new Form(mediator);
+form.addListeners();
