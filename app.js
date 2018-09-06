c3f8eba593ae3e46277ba305f0956df55deab3b5
Turbinist <srudyjju@gmail.com>
2018-09-06 13:00:02 +0300

    Add validator

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ class DataStorage {
 
   add(task) {
     const taskItem = {}
-    taskItem.name = task.name;
-    taskItem.desc = task.desc;
-    taskItem.status = task.status;
+    taskItem.taskName = task.taskName;
+    taskItem.taskDesc = task.taskDesc;
+    taskItem.taskStatus = task.taskStatus;
     this._data.push(taskItem);
     localStorage.setItem(this.privateKey, JSON.stringify(this._data));
   }
 
   update(task) {
     const newList = this._data.map(item => {
-      if (item.name === task.name) {
-        item.status = task.status;
+      if (item.taskName === task.taskName) {
+        item.taskStatus = task.taskStatus;
         return item;
       } else {
         return item;
@@ -31,7 +31,7 @@ class DataStorage {
   }
 
   remove(task) {
-    const newList = this._data.filter(item => item.name !== task.name);
+    const newList = this._data.filter(item => item.taskName !== task.taskName);
     this._data = newList;
     localStorage.setItem(this.privateKey, JSON.stringify(this._data));     
   }
@@ -40,22 +40,23 @@ class DataStorage {
 class Form {
   constructor(mediator) {
     this.mediator = mediator;
-    this.taskName = document.getElementById("taskTitle");
-    this.taskDesc = document.getElementById("taskText");
-    this.taskStatus = document.getElementById("status");
+    this.taskName = "";
+    this.taskDesc = "";
+    this.taskStatus = "";
+    this.taskNameField = document.getElementById("taskTitle");
+    this.taskDescField = document.getElementById("taskText");
+    this.taskStatusField = document.getElementById("status");
     this.addBtn = document.getElementById("addBtn");
     this.add = this.add.bind(this);
     this.addListeners = this.addListeners.bind(this);
+    this.clearFields = this.clearFields.bind(this);
     return (this.addListeners())
   }
 
   addListeners() {
-    this.addBtn.addEventListener("click", (e) => {
-      if (this.taskName.value !== "") {
+    this.addBtn.addEventListener("click", (e) => {      
+      if (this.taskNameField.value !== "") {
         this.add();
-        this.taskName.value = "";
-        this.taskDesc.value = "";
-        this.taskStatus.value = "ToDo";
       }
       else {
         alert("Fill the Task Name field")
@@ -64,16 +65,26 @@ class Form {
   }
 
   add() {
+    this.taskName = this.taskNameField.value;
+    this.taskDesc = this.taskDescField.value;
+    this.taskStatus = this.taskStatusField.value;
     this.mediator.notify(this, "add");
+    this.clearFields();
+  }
+
+  clearFields() {
+    this.taskNameField.value = "";
+    this.taskDescField.value = "";
+    this.taskStatusField.value = "ToDo";
   }
 }
 
 class Task {
   constructor(mediator, sender){
     this.mediator = mediator;
-    this.name = sender.taskName.value;
-    this.desc = sender.taskDesc.value;
-    this.status = sender.taskStatus.value;
+    this.taskName = sender.taskName;
+    this.taskDesc = sender.taskDesc;
+    this.taskStatus = sender.taskStatus;
     this.createElement = this.createElement.bind(this);
     this.removeElement = this.removeElement.bind(this);
     this.setStatus = this.setStatus.bind(this);   
@@ -88,20 +99,20 @@ class Task {
     const cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
     card.appendChild(cardBody);
-    cardBody.innerHTML = `<h5 class="card-title">${this.name}</h5>
-    <p class="card-text">${this.desc}</p>`;
+    cardBody.innerHTML = `<h5 class="card-title">${this.taskName}</h5>
+    <p class="card-text">${this.taskDesc}</p>`;
 
     const form = document.createElement("form");
     const formGroup = document.createElement("div");
     formGroup.classList.add("form-group");
     const label = document.createElement("label");
-    label.setAttribute("for", `${this.name}_status`);
+    label.setAttribute("for", `${this.taskName}_status`);
     label.innerText = "Status";
     const select = document.createElement("select");
     select.classList.add("form-control");
     select.id = `${this.name}_status`;
     select.innerHTML = "<option>ToDo</option><option>Going</option><option>Done</option>  <option>Reject</option>";
-    select.value = this.status;
+    select.value = this.taskStatus;
     select.addEventListener("change", () => {this.setStatus(select.value)});
 
     formGroup.appendChild(label);
@@ -119,7 +130,7 @@ class Task {
   }
 
   setStatus(status) {
-    this.status = status;
+    this.taskStatus = status;
     this.mediator.notify(this, "update");
   }
 
@@ -131,14 +142,39 @@ class Task {
 class Mediator {
   constructor() {
     this.task_list = document.getElementById("taskList");
-    this.dataStorage = new DataStorage(); 
+    this.dataStorage = new DataStorage();
+    this.init = this.init.bind(this);
+    return this.init();
+  }
+
+  init() {
+    window.onload = () => {
+      const renderList = this.dataStorage._data;
+      renderList.forEach(item => {
+        const task = new Task(this, item);
+        this.task_list.appendChild(task.createElement());
+      });
+    }
   }
 
   notify(sender, message) {
-    if (sender instanceof Form && message === "add") {  
-      const task = new Task(this, sender);
-      this.task_list.appendChild(task.createElement());
-      this.dataStorage.add(task);
+    if (sender instanceof Form && message === "add") {
+      const validator = (sender) => {
+        let i = true;
+        this.dataStorage._data.forEach(item => {
+          if (item.taskName === sender.taskName) {          
+            i = false;
+          }                  
+        });
+        return i;        
+      }     
+      if (validator(sender)) {
+        const task = new Task(this, sender);
+        this.task_list.appendChild(task.createElement());
+        this.dataStorage.add(task);
+      } else {
+        alert("Task has already added");
+      }
     } else if (sender instanceof Task && message === "remove") {  
       sender.taskElement.remove();
       this.dataStorage.remove(sender);
